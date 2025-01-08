818b84707308a58880d43d018438558c7b5004fe
Daniel Swery <131551893+DJSW04@users.noreply.github.com>
2025-01-08 16:32:01 +0000

    two-sum.java submit

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -7,8 +7,20 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return nums;
+        int[] twoSum = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < nums.length; j++){
+                if (i == j) {} 
+                else {
+                    if (nums[i] + nums[j] == target){
+                        twoSum[0] = i;
+                        twoSum[1] = j;
+                        return twoSum;
+                    }
+                }
+            }
+        }
+        return twoSum;
     }
 }
 // @lc code=end
-
