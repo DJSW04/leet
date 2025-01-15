7d148120ebb36404095921ca7dd7f8e5e927868e
Daniel Swery <131551893+DJSW04@users.noreply.github.com>
2025-01-15 11:34:56 +0000

    add-two-numbers rev1

--- 2.add-two-numbers.java
+++ 2.add-two-numbers.java
@@ -1,3 +1,5 @@
+
+
 /*
  * @lc app=leetcode id=2 lang=java
  *
@@ -17,8 +19,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode addTwo = new ListNode();
+        String num1 = new String();
+        String num2 = new String();
+        while (l1 != null){
+            num1 += l1.val;
+            l1 = l1.next;
+        }
+        while (l2 != null){
+            num2 += l2.val;
+            l2 = l2.next;
+        }
+        String finalnum = String.valueOf(Integer.parseInt(num1) + Integer.parseInt(num2));
         
+        for (int i = 0; i < finalnum.length(); i++){
+            ListNode temp = new ListNode();
+            temp.val = Character.getNumericValue(finalnum.charAt(i));
+            temp.next = addTwo;
+            addTwo = temp;
+
+        }
+                
+        return addTwo;
     }
+    // public class ListNode {
+    //     int val;
+    //     ListNode next;
+
+    //     ListNode() { }
+
+    //     ListNode(int val) { this.val = val; }
+
+    //     ListNode(int val, ListNode next) {
+    //         this.val = val;
+    //         this.next = next;
+    //     }
+    // }
 }
 // @lc code=end
 
